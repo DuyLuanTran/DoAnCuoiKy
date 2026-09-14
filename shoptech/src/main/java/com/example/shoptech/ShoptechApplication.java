5e35fb090c7432f992ce8f31a06f04f10eb0ae5e
Tran Duy Luan <duyluan5906@gmail.com>
2026-09-14 08:25:58 +0700

    first commit

---
+++ shoptech/src/main/java/com/example/shoptech/ShoptechApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoptechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoptechApplication.class, args);
+	}
+
+}
---
+++ shoptech/src/test/java/com/example/shoptech/ShoptechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoptechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
